7af2b494ee9c55f17b9323e9864755f1a4e035d7
divenon778-web <divenon778@gmail.com>
2026-05-21 08:16:47 +0200

    Update database.py

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ import redis
 import string
 import random
 
-REDIS_URL = os.getenv("!("REDIS_URL", "redis://localhost:6379")
+REDIS_URL = os.getenv("REDIS_URL", "redis://localhost:6379")
 redis_client = redis.from_url(REDIS_URL, decode_responses=True)
 
 ADMIN_KEY = "9001"
